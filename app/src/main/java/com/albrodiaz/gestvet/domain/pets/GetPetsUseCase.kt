20e8c0f152ae4a7d2a77e481effed521f76cb5c2
Cristofer Antonio Berganza Portillo <antoniocristofer577@gmail.com>
2024-10-21 13:56:47 -0600

    Creacion Pets

---
+++ app/src/main/java/com/albrodiaz/gestvet/domain/pets/GetPetsUseCase.kt
@@ -0,0 +1,8 @@
+package com.albrodiaz.gestvet.domain.pets
+
+import com.albrodiaz.gestvet.data.network.PetService
+import javax.inject.Inject
+
+class GetPetsUseCase @Inject constructor(private val petService: PetService) {
+    operator fun invoke() = petService.pets()
+}
