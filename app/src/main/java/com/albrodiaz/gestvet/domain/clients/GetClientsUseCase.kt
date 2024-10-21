9e9de9976e17979358614cb7bd10312f48d7d65d
Cristofer Antonio Berganza Portillo <antoniocristofer577@gmail.com>
2024-10-20 22:45:06 -0600

    Creacion Clients

---
+++ app/src/main/java/com/albrodiaz/gestvet/domain/clients/GetClientsUseCase.kt
@@ -0,0 +1,11 @@
+package com.albrodiaz.gestvet.domain.clients
+
+import com.albrodiaz.gestvet.data.network.ClientService
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetClientsUseCase @Inject constructor(private val clientService: ClientService) {
+    operator fun invoke(): Flow<QuerySnapshot> = clientService.clients()
+
+}
