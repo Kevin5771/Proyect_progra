b4e547d73a5064400b9e11148b16f9ab652d51e3
jU1fr <jlechugao@miumg.edu.gt>
2024-10-29 17:05:43 -0600

    Custom data picker and connectivity

---
+++ app/src/main/java/com/gestvet/gestvet/core/ConnectivityService.kt
@@ -0,0 +1,41 @@
+package com.gestvet.gestvet.core
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+sealed class ConnectionState {
+    object Available: ConnectionState()
+    object Unavailable: ConnectionState()
+}
+
+class NetworkConnectivity(context: Context) {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    fun getConnectivity() = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                launch { send(ConnectionState.Available) }
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                launch { send(ConnectionState.Unavailable) }
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                launch { send(ConnectionState.Unavailable) }
+            }
+        }
+        connectivityManager.registerDefaultNetworkCallback(callback)
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }
+}
