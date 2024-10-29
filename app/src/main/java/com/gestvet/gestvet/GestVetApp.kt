de8cc2fe50574a9f3cebcbf5553345f40939efbf
Kevin5771 <kevin577gutierrez@gmail.com>
2024-10-28 21:58:06 -0600

    first commit

---
+++ app/src/main/java/com/gestvet/gestvet/GestVetApp.kt
@@ -0,0 +1,8 @@
+package com.gestvet.gestvet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GestVetApp: Application() {
+}
---
+++ app/src/main/java/com/gestvet/gestvet/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.gestvet.gestvet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.gestvet.gestvet.ui.features.login.view.HomeScreen
+import com.gestvet.gestvet.ui.theme.GestVetTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GestVetTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
