4e5a4d5c1dc6d8a431104fe24bc1254c180d75a0
dani2sandoval <msandovalm15@miumg.edu.gt>
2024-10-29 18:59:22 -0600

    Client & Users

---
+++ app/src/main/java/com/gestvet/gestvet/data/network/ClientService.kt
@@ -0,0 +1,43 @@
+package com.gestvet.gestvet.data.network
+
+import android.util.Log
+import com.gestvet.gestvet.core.extensions.getDocumentFlow
+import com.gestvet.gestvet.core.extensions.getFlow
+import com.gestvet.gestvet.ui.features.home.models.ClientsModel
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class ClientService @Inject constructor(firebase: FirebaseClient) {
+
+    private val currentUser = firebase.auth.currentUser?.email
+
+    companion object {
+        const val CLIENTS_TAG = "clients"
+    }
+
+    private val clientReference = firebase.dataBase.collection("$currentUser/management/clients")
+
+    fun clients() = clientReference
+        .orderBy("lastname", Query.Direction.ASCENDING).getFlow()
+
+    fun getClientById(id: Long) = clientReference.whereEqualTo("id", id).getDocumentFlow()
+
+    suspend fun addClient(clientsModel: ClientsModel) {
+        clientReference.document("${clientsModel.id}")
+            .set(clientsModel).await()
+    }
+
+    suspend fun deleteClient(id: Long) {
+        clientReference.document("$id")
+            .delete()
+            .addOnSuccessListener {
+                Log.i(CLIENTS_TAG, "Cita borrada con éxito")
+            }
+            .addOnCanceledListener {
+                Log.e(CLIENTS_TAG, "Error al borrar la cita")
+            }
+            .await()
+    }
+
+}
---
+++ app/src/main/java/com/gestvet/gestvet/data/network/UserService.kt
@@ -0,0 +1,43 @@
+package com.gestvet.gestvet.data.network
+
+import android.util.Log
+import com.gestvet.gestvet.ui.features.login.model.User
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class UserService @Inject constructor(private val firebaseClient: FirebaseClient) {
+
+    companion object {
+        const val USER_TAG = "users"
+    }
+
+    private val currentUser = firebaseClient.auth.currentUser?.email
+
+    suspend fun createUser(user: User) = runCatching {
+
+        val registerUser = hashMapOf(
+            "name" to user.name,
+            "email" to user.email,
+            "id" to user.id
+        )
+
+        firebaseClient.dataBase.collection("${user.email}").document("settings")
+            .set(registerUser)
+            .await()
+    }.isSuccess
+
+    suspend fun getUserData() = callbackFlow {
+        val data = firebaseClient.dataBase.collection(currentUser.orEmpty()).document("settings")
+            .addSnapshotListener { value, error ->
+                error?.let {
+                    Log.e(USER_TAG, "Error al recuperar el usuario")
+                }
+                value?.let {
+                    trySend(it)
+                }
+            }
+        awaitClose { data.remove() }
+    }
+}
