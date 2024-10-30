c7c1a72bf1e5215ff623b7476d128d797ab64e1f
Kevin5771 <kevin577gutierrez@gmail.com>
2024-10-29 19:23:22 -0600

    Componentes Faltantes

---
+++ app/src/main/java/com/gestvet/gestvet/ui/features/components/Cards.kt
@@ -0,0 +1,23 @@
+package com.gestvet.gestvet.ui.features.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.gestvet.gestvet.ui.theme.*
+
+@Composable
+fun DetailCard(content: @Composable ()-> Unit) {
+    val containerColor = if (isSystemInDarkTheme()) md_theme_dark_surface else md_theme_light_inverseOnSurface
+    Card(
+        modifier = Modifier.padding(12.dp),
+        colors = CardDefaults.cardColors(containerColor = containerColor),
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = Shapes.large
+    ) {
+        content()
+    }
+}
