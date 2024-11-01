57bbf29c8b7b45e38522391ab699832da24431c3
Kevin5771 <kevin577gutierrez@gmail.com>
2024-10-31 22:25:58 -0600

    Models Foro

---
+++ app/src/main/java/com/gestvet/gestvet/ui/features/home/models/PostForum.kt
@@ -0,0 +1,8 @@
+package com.gestvet.gestvet.ui.features.home.models
+
+data class PostForum(
+    var id: String = "",
+    var title: String = "",
+    var content: String = "",
+    var likes: Int = 0
+)
---
+++ app/src/main/java/com/gestvet/gestvet/ui/features/home/models/Review.kt
@@ -0,0 +1,6 @@
+package com.gestvet.gestvet.ui.features.home.models
+
+data class Review(
+    val comment: String = "",
+    val rating: Int = 0
+)
